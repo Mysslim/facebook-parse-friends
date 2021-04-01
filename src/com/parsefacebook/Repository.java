4fc59cb4994c41db5a5459b4af1eef959ed1bd5e
Vodyanov Andrey <50964980+Mysslim@users.noreply.github.com>
2021-04-01 13:56:34 +0300

    Delete src directory

--- src/com/parsefacebook/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.parsefacebook;
-
-import java.util.List;
-
-public interface Repository {
-    void insert(List<User> users);
-    boolean update(User oldUser, User newUser);
-    boolean delete(User user);
-}
