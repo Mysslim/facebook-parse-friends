81ba0655c97e8bfbe25681345275c04663083970
Mysslim <vodyanov.a.a.20@gmail.com>
2021-04-08 11:14:24 +0300

    refactoring code

--- mainclass.py
+++ mainclass.py
@@ -4,8 +4,10 @@ import logging
 import configparser
 
 from selenium import webdriver
-from selenium.common.exceptions import InvalidSelectorException, NoSuchElementException, StaleElementReferenceException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from webdriver_manager.firefox import GeckoDriverManager
@@ -14,17 +16,18 @@ from constants import *
 
 class Main:
     def __init__(self):
-        self.__config = self.__setup_config()
-        self.__driver = self.__setup_driver()
-        self.setup_logger()
+        self.__setup_config()
+        self.__setup_logger()
+        self.__setup_driver()
+        self.__count_of_friends = 0
 
     def get_config(self):
         return self.__config
 
     def get_count_friend(self):
-        return COUNT_OF_FRIEND
+        return self.__count_of_friends
 
-    def setup_logger(self):
+    def __setup_logger(self):
         logging.basicConfig(
             filename = self.__config["Logger"]["FOLDER_FOR_LOG"] + "/" + self.__config["Logger"]["NAME_LOG_FILE"],
             filemode = self.__config["Logger"]["FILE_MODE_LOG"],
@@ -32,9 +35,9 @@ class Main:
             format = "%(asctime)s-%(levelname)s-%(message)s")
 
     def __setup_config(self):
-        config = configparser.ConfigParser()
+        self.__config = configparser.ConfigParser()
         try:
-            config.read("config.ini")
+             self.__config.read("config.ini")
 
         except FileNotFoundError:
             print("config not found. Stop working")
@@ -44,30 +47,33 @@ class Main:
             print("an unexpected exception occurred")
             sys.exit()
 
-        return config
-
-    def get_path_to(self, str: str) :
-        return os.path.abspath(str)
-
     def __setup_driver(self):
         logging.debug("start setup driver")
         options = webdriver.FirefoxOptions()
 
         executable_path = GeckoDriverManager(path = self.get_path_to("./")).install()
         logging.debug("check new version driver and try install, return path to driver, path: {0}".format(executable_path))
-
-        options.add_argument("--start-maximized")   # fullscreen
         options.add_argument("--no-sandbox")
         options.add_argument("--single-process")
         options.add_argument("--disable-notifications")
         logging.debug("add arguments")
 
-        driver = webdriver.Firefox(executable_path=executable_path, options=options, log_path=self.__config["Logger"]["FOLDER_FOR_LOG"] + "/geckodriver.log")
-        driver.delete_all_cookies()
+        self.__driver = webdriver.Firefox(executable_path=executable_path, options=options, log_path=self.__config["Logger"]["FOLDER_FOR_LOG"] + "/geckodriver.log")
+        self.__driver.delete_all_cookies()
+        self.__driver.maximize_window()
         logging.debug("end setup driver")
 
-        return driver
+    def get_path_to(self, str: str) :
+        return os.path.abspath(str)
 
+    def set_count_friend(self, count : int):
+        self.__count_of_friends = count
+
+    def exit(self):
+        self.__driver.close()
+        sys.exit()
+
+    
     def check_argv(self):
         try:
             script, login, passwd = sys.argv
@@ -81,140 +87,93 @@ class Main:
         wait = WebDriverWait(self.__driver, int(self.__config["Timer"]["SECOND"]))
         wait.until(rule)
 
-    def login_in_facebook(self):
-        self.check_argv()
-        script, login, passwd = sys.argv
-
-        if self.__driver.current_url != "https://www.facebook.com/":
-            logging.warning("url is not facebook, change url")
-            self.__driver.get("https://www.facebook.com/")
-
-        self.wait_by_the_rule(expected_conditions.element_to_be_clickable((By.ID, "email")))
-
-        text_box_login = self.__driver.find_element(By.ID, SELECTOR_FOR_LOGIN)
-        text_box_login.send_keys(login)
-
-        text_box_login = self.__driver.find_element(By.ID, SELECTOR_FOR_PASSWD)
-        text_box_login.send_keys(passwd)
-
-        button_login = self.__driver.find_element(By.TAG_NAME, SELECTOR_FOR_BUTTON)
-        button_login.click()
-
-    def scrollInBottom(self):
-        self.wait_by_the_rule(expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR, "div")))
-
-        javascript = """
-        for(let i = 0; i < 50; i++) {
-            scroll()
-            setTimeout(scroll, 2000)
-            
-        }
-        function scroll() { 
-            window.scrollBy(0, 3000);
-        }
-        """
-
-        logging.debug("make script")
-        self.__driver.execute_script(javascript)
-        logging.debug("execute script")
-
-    def get_locator_path(self, panel, path_to):
-        css_path = "{0}[class='{1}'] {2}".format(
-            panel.tag_name, 
-            panel.get_attribute("class"),
-            path_to)
-
-        return css_path
-
-    def get_panel_friend(self):
-        panel = None
-
-        self.wait_by_the_rule(expected_conditions.visibility_of_element_located((By.XPATH, XPATH_TO_PANEL_FRIENDS_FIREFOX)))
-        panel = self.__driver.find_element(By.XPATH, XPATH_TO_PANEL_FRIENDS_FIREFOX)
-        
-        return panel
-
-    def get_web_elements(self, by: By, path: str, panel):
-        web_elements = None
-
+    def wait_for_item_load(self, by: By, path: str, element : str):
         try:
-            logging.debug("waiting load all friends")
-            self.wait_by_the_rule(expected_conditions.number_of_windows_to_be(((by, path), self.get_count_friend())))
+            logging.debug("waiting {0} form".format(element))
+            self.wait_by_the_rule(expected_conditions.visibility_of_element_located((by, path)))
+
+        except NoSuchElementException:
+            logging.error("{0} not found exit".format(element))
 
         except TimeoutException:
-            logging.error("not all friends are loaded")
+            logging.error("timeout expired for {0}, exit".format(element))
+            self.exit()
 
-        except InvalidSelectorException:
-            logging.error("incorrect selector, return None")
-            return None
+    def wait_until_item_is_clickable(self, by: By, path: str, element: str):
         try:
-            web_elements = panel.find_elements(by, path)
-        
-        except StaleElementReferenceException:
-            logging.error("panelFriend is out of date, update panelFriend.")
-            panel = self.get_panel_friend()
-            web_elements = panel.find_elements(by, path)
-         
-        return web_elements
+            logging.debug("waiting until {0} is clickable".format(element))
+            self.wait_by_the_rule(expected_conditions.element_to_be_clickable((by, path)))
 
-    def get_friends(self):
+        except NoSuchElementException:
+            logging.error("{0} not found exit".format(element))
+
+        except TimeoutException:
+            logging.error("timeout expired for {0}, exit".format(element))
+            self.exit()
+
+    def login_in_facebook(self):
+        self.check_argv()
+        script_name, login, passwd = sys.argv
+
+        self.wait_until_item_is_clickable(By.ID, SELECTOR_FOR_LOGIN, "text_box_login")
+
+        self.__driver.find_element(By.ID, SELECTOR_FOR_LOGIN).send_keys(login)
+        self.__driver.find_element(By.ID, SELECTOR_FOR_PASSWD).send_keys(passwd)
+        self.__driver.find_element(By.TAG_NAME, SELECTOR_FOR_BUTTON).click()
+
+    def get_path_to_friend_in_panel(self, index : int):
+        if index <= 0:
+            logging.warning("friend index not be less 0")
+            index = 1
+        return "{0}[{1}]".format(PATH_TO_FRIEND, index) 
+
+    def move_to_myprofile(self):
+        self.wait_for_item_load(By.CSS_SELECTOR, PATH_TO_MYPROFILE, "button_my_profile")
+        self.__driver.find_element(By.CSS_SELECTOR, PATH_TO_MYPROFILE).click()
+
+    def move_to_friends(self):
+        self.wait_for_item_load(By.XPATH, PATH_TO_DROP_DOWN_FORM, "drop_down_form")
+        drop_down_form = self.__driver.find_element(By.XPATH, PATH_TO_DROP_DOWN_FORM)
+        drop_down_form.click()
+
+        self.wait_for_item_load(By.XPATH, PATH_TO_COUNT_FRIENDS, "count_friend")
+        self.set_count_friend(int(self.__driver.find_element(By.XPATH, PATH_TO_COUNT_FRIENDS).text))
+
+        self.wait_for_item_load(By.XPATH, PATH_TO_BUTTON_FRIENS, "button_friends")
+        self.__driver.find_elements(By.XPATH, PATH_TO_BUTTON_FRIENS)[0].click()
+
+    def get_DOM_friends(self):
+        web_element_friends = []
+        
+        while len(web_element_friends) < self.get_count_friend():
+            self.__driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+            self.wait_for_item_load(By.CSS_SELECTOR, PATH_TO_FRIEND, "friends")
+            web_element_friends = self.__driver.find_elements(By.CSS_SELECTOR, PATH_TO_FRIEND)
+
+        return web_element_friends 
+
+    def get_info_about_friends(self):
+        web_element_friends = self.get_DOM_friends()
         friends = []
-        fullnames = self.get_fullname_friends()
-        profiles = self.get_profiles_friends()
-        avatars = self.get_avatars_friends()
 
-        count = len(fullnames)
+        for friend in web_element_friends:
+            info_friend = []
+            info_friend.append(friend.find_element(By.CSS_SELECTOR, PATH_ADDRESS_IN_PROFILE).get_attribute("href"))
+            info_friend.append(friend.find_element(By.CSS_SELECTOR, PATH_FULLNAME).text)
+            
+            friends.append(info_friend)
 
-        for friend in range(count):
-            friends.append([fullnames[friend], profiles[friend], avatars[friend]])
-        
         return friends
-
-    def get_fullname_friends(self):
-        logging.debug("start getting fullname friends")
-        panel = self.get_panel_friend()
-        
-        elements = self.get_web_elements(By.CSS_SELECTOR, self.get_locator_path(panel, PATH_FULLNAME_ACTIVE), panel)
-
-        fullnames = []
-        for element in elements:
-            fullnames.append(element.text)
-
-        logging.debug("return")
-        return fullnames
-    
-    def get_profiles_friends(self):
-        logging.debug("start getting profile friends")
-        panel = self.get_panel_friend()
-        
-        elements = self.get_web_elements(By.CSS_SELECTOR, self.get_locator_path(panel, PATH_ADDRESS_IN_PROFILE), panel)
-        profiles = []
-        for element in elements:
-            profiles.append(element.get_attribute("href"))
-
-        logging.debug("return")
-        return profiles
-
-    def get_avatars_friends(self):
-        logging.debug("start getting avatar friends")
-        panel = self.get_panel_friend()
-        
-        elements = self.get_web_elements(By.CSS_SELECTOR, self.get_locator_path(panel, PATH_IMG_ACTIVE), panel)
-        avatars = []
-        for element in elements:
-            avatars.append(element.get_attribute("src"))
-
-        logging.debug("return")
-        return avatars
 
     def workflow(self):
         
         self.__driver.get("https://www.facebook.com/")
         self.login_in_facebook()
-        self.__driver.get(self.__config["url"]["URL_TO_FRIENDS"])
-        self.scrollInBottom()
-        friends = self.get_friends()
-        
+        self.move_to_myprofile()
+        self.move_to_friends()
+        friends = self.get_info_about_friends()
+
         repository = CSVRepository(self.get_config())
         repository.insert(friends)
 
